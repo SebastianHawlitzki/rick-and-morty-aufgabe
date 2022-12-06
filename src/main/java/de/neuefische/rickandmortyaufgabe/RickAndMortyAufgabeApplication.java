a21861a2ed26c9d15bd72b6e1baf6dbc0406d9ab
sebastianhawlitzki <business.hawlitzki@gmail.com>
2022-12-06 21:11:07 +0100

    init: BIG BANG

---
+++ src/main/java/de/neuefische/rickandmortyaufgabe/RickAndMortyAufgabeApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.rickandmortyaufgabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyAufgabeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortyAufgabeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/rickandmortyaufgabe/RickAndMortyAufgabeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.rickandmortyaufgabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyAufgabeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
